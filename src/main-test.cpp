e1d4b3635ad108ec3e90010d9ce243effc677399
Jerson Miranda <56894248+jmiranda1996@users.noreply.github.com>
2023-06-19 14:30:41 +0000

    chore: move main file

---
+++ src/main-test.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// #include "domain/entities/Client.h"
+// #include "domain/shared/User.h"
+
+// using namespace std;
+// // using namespace td;
+
+// int main(int argc, char *argv[]) {
+    
+//     cout << "Turismo Dias Core" << endl;
+
+//     tdc::domain::shared::User * newClient = new tdc::domain::entities::Client();
+//     newClient->setUsername("Jerson Miranda");
+
+//     cout << newClient->getUsername() << endl;
+
+//     cin.get();
+// }
